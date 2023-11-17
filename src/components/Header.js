f70ace1b8fc323c44d0936d7340dbde9c2dbf46d
praju4 <prajwalkshatriya47@gmail.com>
2023-11-17 14:49:28 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import Image from "./images/2.jpg"
+// import Image from ".images/2.jpg"
 
 
 const Header = () => {
@@ -14,7 +14,7 @@ const Header = () => {
 const MainContent = () => {
   return (
     <main>
-      <img src={Image} width="700px" height="400px" alt="Not found/"></img>
+      {/* <img src={Image} width="700px" height="400px" alt="Not found/"></img> */}
       <p><i><h3>Leonardo Wilhelm DiCaprio (/diˈkæprioʊ, dɪ-/; Italian: [diˈkaːprjo]; born November 11, 1974) is an American actor and film producer. Known for his work in biographical and period films, he is the recipient of numerous accolades, including an Academy Award, a British Academy Film Award, and three Golden Globe Awards. As of 2019, his films have grossed over $7.2 billion worldwide, and he has been placed eight times in annual rankings of the world's highest-paid actors.
 
 Born in Los Angeles, DiCaprio began his career in the late 1980s by appearing in television commercials. In the early 1990s, he had recurring roles in various television shows, such as the sitcom Parenthood, and had his first major film part as author Tobias Wolff in This Boy's Life (1993). He received critical acclaim and his first Academy Award and Golden Globe Award nominations for his performance as a developmentally disabled boy in What's Eating Gilbert Grape (1993). DiCaprio achieved international stardom with the star-crossed romances Romeo + Juliet (1996) and Titanic (1997). After the latter became the highest-grossing film in the world at the time, he reduced his workload for a few years. In an attempt to shed his image of a romantic hero, DiCaprio sought roles in other genres, including the 2002 crime dramas Catch Me If You Can and Gangs of New York; the latter marked the first of his many successful collaborations with director Martin Scorsese.
